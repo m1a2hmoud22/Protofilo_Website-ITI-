91caad41568cfeda83466231b09d1bd20acb58ec
mahmoud mohmed <bp159123147@gmail.com>
2025-07-20 01:19:29 +0200

    Js

---
+++ index.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('section[id]');
+        const navLinks = document.querySelectorAll('.navaction a');
+
+        window.addEventListener('scroll', () => {
+            let current = '';
+            sections.forEach(section => {
+                const sectionTop = section.offsetTop;
+                if (pageYOffset >= sectionTop - 150) {
+                    current = section.getAttribute('id');
+                }
+            });
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').substring(1) === current) {
+                    link.classList.add('active');
+                }
+            });
+        });
